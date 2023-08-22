6f3b00bde547012c159d3c2dd00a279ed7d2ff7f
Avintha Fonseka <avintha@Avinthas-MBP-2.lan>
2023-08-21 23:30:39 -0400

    Initial Commit

---
+++ Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BlazorTodoApp.Models
+{
+	public class Todo
+	{
+        public string Title { get; set; }
+        public DateTime DueBy { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
+
